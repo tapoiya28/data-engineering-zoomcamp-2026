4dffd5f234790e2d0d6e94f9bd85488f99d2a610
Tapo <23127534@student.hcmus.edu.vn.com>
2026-01-20 10:10:19 +0700

    dag with gcp completed

--- W02-orchestration/dags/ingest_taxi_gcp.py
+++ W02-orchestration/dags/ingest_taxi_gcp.py
@@ -16,18 +16,16 @@ prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases'
 PROJECT_ID = os.getenv('GCP_PROJECT_ID', 'dataengineer-zoomcamp-484202')
 DATASET_ID = 'ny_taxi'
 BUCKET_NAME = 'ny-taxi-data-lake'
-OBJECT_NAME = ''
 
-
-def extract_yellow(**context):
+def extract_yellow():
     # context = get_current_context()
     # data_start = context["data_interval_start"]
     year = 2021
     month = 1
 
     # read csv
-    FILENAME = f'yellow_tripdata_{year}-{month:02}.csv.gz'
-    url = f'{prefix}/download/yellow/{FILENAME}'
+    filename = f'yellow_tripdata_{year}-{month:02}.csv.gz'
+    url = f'{prefix}/download/yellow/{filename}'
     
     response = requests.get(url)
     response.raise_for_status()
@@ -38,27 +36,45 @@ def extract_yellow(**context):
 
     hook = GCSHook(gcp_conn_id='gcp-conn-id-1')
     bucket_name = BUCKET_NAME
-    OBJECT_NAME = f"yellow/{FILENAME.replace('.csv.gz', '.csv')}"
-    print(OBJECT_NAME)
+    object_name = f"yellow/{filename.replace('.csv.gz', '.csv')}"
+    print(object_name)
     hook.upload(bucket_name=bucket_name,
-                object_name=OBJECT_NAME,
+                object_name=object_name,
                 mime_type='text/csv',
                 data=decompressed_csv,
                 gzip=False
                 )
     
-    return OBJECT_NAME
+    return filename.replace('.csv.gz', '.csv')
                 
 
 def extract_green():
-    context = get_current_context()
-    data_start = context["data_interval_start"]
+    # context = get_current_context()
+    # data_start = context["data_interval_start"]
+    year = 2021
+    month = 1
 
-    year = data_start.year
-    month = data_start.month
+    # read csv
+    filename = f'green_tripdata_{year}-{month:02}.csv.gz'
+    url = f'{prefix}/download/green/{filename}'
+    
+    response = requests.get(url)
+    response.raise_for_status()
 
-    FILENAME = f'green_tripdata_{year}-{month:02}.csv.gz'
-    url = f'{prefix}/download/green/{FILENAME}'
+    # decompress locally so bucket stores plain CSV
+    with gzip.GzipFile(fileobj=io.BytesIO(response.content)) as gz:
+        decompressed_csv = gz.read()
 
-    OBJECT_NAME = f"green/{FILENAME.replace('.csv.gz', '.csv')}"
+    hook = GCSHook(gcp_conn_id='gcp-conn-id-1')
+    bucket_name = BUCKET_NAME
+    object_name = f"green/{filename.replace('.csv.gz', '.csv')}"
+    print(object_name)
+    hook.upload(bucket_name=bucket_name,
+                object_name=object_name,
+                mime_type='text/csv',
+                data=decompressed_csv,
+                gzip=False
+                )
+    
+    return filename.replace('.csv.gz', '.csv')
     
